eba3501d4cb1d58c0f093023d10ee3c219f24691
justinwoo <moomoowoo@gmail.com>
2018-10-08 00:22:15 +0300

    basic build, test

--- app/Main.hs
+++ app/Main.hs
@@ -6,20 +6,42 @@ import qualified Turtle as T
 
 -- | Commands that this program handles
 data Command
-  = Hello
+  = Build
+  | Test
 
-hello :: IO ()
-hello = do
-  putStrLn "hello"
+run :: Command -> IO ()
+run Build = do
+  let
+    cmd = "psc-package build"
+  code <- T.shell cmd T.empty
+  case code of
+    T.ExitSuccess ->
+      T.echo . T.unsafeTextToLine $ "Build succeeded"
+    T.ExitFailure n ->
+      T.die $ "Failed to build: " <> T.repr n
+
+run Test = do
+  let
+    cmd
+       = "psc-package build -- "
+      <> "test/Main.purs"
+      <> "&& node -e 'require(\"./output/Test.Main\").main()'"
+  code <- T.shell cmd T.empty
+  case code of
+    T.ExitSuccess ->
+      T.echo . T.unsafeTextToLine $ "Tests succeeded."
+    T.ExitFailure n ->
+      T.die $ "Tests failed: " <> T.repr n
 
 parser :: T.Parser Command
 parser
-      = Hello <$ localSetup
+      = Build <$ build
+  T.<|> Test <$ test
   where
-    localSetup = T.subcommand "hello" "hello" $ pure ()
+    build = T.subcommand "build" "build" $ pure ()
+    test = T.subcommand "test" "test" $ pure ()
 
 main :: IO ()
 main = do
   options <- T.options "purp" parser
-  case options of
-    Hello -> hello
+  run options
